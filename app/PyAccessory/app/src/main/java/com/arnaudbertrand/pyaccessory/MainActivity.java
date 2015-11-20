18230adcb36d77c66df71b4b591249142a6f60d5
Arn-O <rnd.bertrand@gmail.com>
2015-11-20 13:27:37 +0100

    app logic and handlers

--- app/PyAccessory/app/src/main/java/com/arnaudbertrand/pyaccessory/MainActivity.java
+++ app/PyAccessory/app/src/main/java/com/arnaudbertrand/pyaccessory/MainActivity.java
@@ -1,6 +1,9 @@
 package com.arnaudbertrand.pyaccessory;
 
+
+
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,24 +11,141 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.text.method.ScrollingMovementMethod;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbAccessory;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mText;
+    ParcelFileDescriptor mFileDescriptor;
+    String mMessage;
+    String mMessage2;
+    String TAG = "DebugPy";
+
+    UsbManager mUsbManager;
+    UsbAccessory mAccessory;
+
+    FileInputStream mInputStream;
+    FileOutputStream mOutputStream;
+
+    Runnable mUpdateUI = new Runnable() {
+        @Override
+        public void run() {
+            mText.append(mMessage);
+        }
+    };
+
+    Runnable mUpdateUI2 = new Runnable() {
+        @Override
+        public void run() {
+            mText.append(mMessage2);
+        }
+    };
+
+    /** Called when the user clicks the Decrease or Increase button */
+    public void sendMessage(View view) {
+
+        char direction = 0;
+
+        switch (view.getId()) {
+            case R.id.actuator_decrease:
+                direction = '1';
+                break;
+            case R.id.actuator_increase:
+                direction = '0';
+                break;
+        }
+
+        byte[] buffer = new byte[5];
+        buffer[0] = (byte) 'A';
+        for (int i = 1; i < 5; i++) {
+            buffer[i] = (byte) direction;
+        }
+
+        mMessage2 = "<<< ";
+        try {
+            mOutputStream.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+            mMessage2 += "Send error";
+        }
+        String msg = new String(buffer);
+        mMessage2 += msg + System.getProperty("line.separator");
+        mText.post(mUpdateUI2);
+
+    }
+
+    Runnable mListenerTask = new Runnable() {
+        @Override
+        public void run() {
+
+            byte[] buffer = new byte[5];
+            int ret;
+
+            try {
+                mMessage = ">>> ";
+                ret = mInputStream.read(buffer);
+                if (ret == 5) {
+                    String msg = new String(buffer);
+                    mMessage += msg;
+                } else {
+                    mMessage += "Read error";
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                mMessage += "Read error";
+            }
+
+            mMessage += System.getProperty("line.separator");
+            mText.post(mUpdateUI);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            new Thread(this).start();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mText = (TextView) findViewById(R.id.display_area);
+        mText.setMovementMethod(new ScrollingMovementMethod());
+
+        Intent intent = getIntent();
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mAccessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+
+        if (mAccessory == null) {
+            mText.append("Not started by the accessory directly" +
+                    System.getProperty("line.separator"));
+            return;
+        }
+
+        Log.v(TAG, mAccessory.toString());
+        mFileDescriptor = mUsbManager.openAccessory(mAccessory);
+        if (mFileDescriptor != null) {
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            mInputStream = new FileInputStream(fd);
+            mOutputStream = new FileOutputStream(fd);
+        }
+        Log.v(TAG, mFileDescriptor.toString());
+        new Thread(mListenerTask).start();
     }
 
     @Override
@@ -33,20 +153,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
